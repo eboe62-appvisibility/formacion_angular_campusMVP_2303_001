ac84245ef00436de31559fb13856bf0aaf162269
Enrique Orellana <eboe62@hotmail.com>
2022-10-06 23:38:18 +0200

    added 04_Componentes. issue #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventosComponent } from './eventos/eventos.component';
+import { EventoComponent } from './evento/evento.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EventosComponent, EventoComponent
   ],
   imports: [
     BrowserModule
