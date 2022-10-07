2254909b5aaecef50d1c86a41aba99e26a652486
Quique <eboe62@hotmail.com>
2022-10-07 02:30:48 +0200

    fixed 04_Componentes. issue#4

--- src/app/evento/evento.component.ts
+++ src/app/evento/evento.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-evento',
+  //templateUrl: './evento.component.html',
+  //template:"<p>Aquí iria un evento</p>",
   templateUrl: './evento.component.html',
+  //styleUrls: ['./evento.component.css']
+  //styles:["p{background-color:red;}"]
   styleUrls: ['./evento.component.css']
 })
 export class EventoComponent implements OnInit {
