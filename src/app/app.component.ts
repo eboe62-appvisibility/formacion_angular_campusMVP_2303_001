418a811ddd46a70e4287797e080b741c167a3ea0
Quique <eboe62@hotmail.com>
2022-12-18 21:47:41 +0100

    Implantación Firebase: npm install –save firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import firebase from  'firebase/compat/app';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TITLE: ';
 
   constructor(){
+  }
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: "AIzaSyArHL0y1cNuDEG_2n3nngjtV4fwNm4u-jY",
+      autDomain: "apptravel01-d12e0",
+    })
   }
 
 }
